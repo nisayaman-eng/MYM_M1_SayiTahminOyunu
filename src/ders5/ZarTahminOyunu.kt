f81ae9c158ebae005ad3cb934ad54635be91c720
Nisa Nur Yaman <nasanar9@gmail.com>
2026-07-17 05:36:23 +0300

    Ders5: Ödev Zar Tahmin Oyunu Yapıldı

--- src/ders5/ZarTahminOyunu.kt
+++ src/ders5/ZarTahminOyunu.kt
@@ -4,19 +4,19 @@ fun main() {
     print("1-6 arasında bir zar tahmini yazın: ")
     val tahmin = readln().toIntOrNull()
 
-    if (tahmin == null){
+    if (tahmin == null) {
         println("Lütfen bir tam sayı değeri girin!")
         return
     }
 
-    if (tahmin > 6 || tahmin < 1){
+    if (tahmin > 6 || tahmin < 1) {
         println("Zar sadece 1, 2, 3, 4, 5 veya 6 gelebilir!")
-    }else {
+    } else {
         val zar = (1..6).random()
 
-        if (tahmin == zar){
+        if (tahmin == zar) {
             println("Tebrikler, Zar $zar geldi ve doğru bildiniz.")
-        }else{
+        } else {
             println("Maalesef bilemediniz. zar $zar gelmişti.")
         }
 
